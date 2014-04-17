e31e9fe6e2df41eb5a11c57da5772c69c48aa6e6
loopasam <samuel.croset@gmail.com>
2014-04-17 14:51:57 +0100

    exception handling

--- app/jobs/SampleJob.java
+++ app/jobs/SampleJob.java
@@ -14,7 +14,7 @@ public class SampleJob extends Job {
 		Logger.info("Getting data...");
 
 		Fixtures.delete(Citation.class);
-		
+
 		Connection c = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -24,41 +24,36 @@ public class SampleJob extends Job {
 			Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
 			c = DriverManager.getConnection(url, "CDB_READ_CHEMBL", "readonly");
 			pstmt = c.prepareStatement("SELECT * from CDB.CITATIONS where source = 'MED'");
-//			pstmt = c.prepareStatement("select * from CDB.CITATIONS where external_id = '20814568'");
-			
+			//			pstmt = c.prepareStatement("select * from CDB.CITATIONS where external_id = '20814568'");
+
 			//pstmt.setMaxRows(1000);
 			rs = pstmt.executeQuery();
 			int counter = 1;
 			int total = 23742757;
-			
+
 			while (rs.next()) {
 				String title = rs.getString("TITLE_SPECIAL_CHAR");
 				if(title == null){
 					title = rs.getString("TITLE");
 				}
-				
+
 				String abstractText = rs.getString("ABSTRACT");
-				
+
 				if(abstractText == null){
 					abstractText = rs.getString("ABSTRACT_LONG");
 				}
 				String pmid = rs.getString("EXTERNAL_ID");
 				String created = rs.getString("CREATED");
-				
+
 				Logger.info("Record (PMID: " + pmid + ") - " + counter + "/" + total);
 				counter++;
-				try{
-					new Citation(pmid, title, abstractText, created).save();
-				}catch(Exception e){
-					e.printStackTrace();
-				}finally{
-					if (counter%1000 == 0) {
-						Citation.em().flush();
-						Citation.em().clear();
-					}
-				}
 				
-				
+				new Citation(pmid, title, abstractText, created).save();
+				if (counter%1000 == 0) {
+					Citation.em().flush();
+					Citation.em().clear();
+				}				
+
 			}
 
 		}catch(Exception e){
