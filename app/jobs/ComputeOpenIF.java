561c0e18f14b7604b924e4c1b4fe7007da6743b5
loopasam <samuel.croset@gmail.com>
2014-07-01 16:13:08 +0100

    update openIF computation

--- app/jobs/ComputeOpenIF.java
+++ app/jobs/ComputeOpenIF.java
@@ -9,6 +9,7 @@ import com.google.common.base.Stopwatch;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import models.Citation;
@@ -36,81 +37,60 @@ public class ComputeOpenIF extends Job {
         //in 2013 have been considered.
         //B = # of articles published by the journal in 2011 and 2012.
         //IF(2013) = A/B
-        List<Journal> journals = Journal.findAll();
-        int counter = 0;
-        int total = journals.size();
-
-        //TODO do not hardcode this part
         int now = Integer.parseInt((String) play.Play.configuration.get("analysis.year"));
-        int y1 = now - 1;
         int y2 = now - 2;
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Date start2y = sdf.parse("01/01/" + y1);
-        Date end2y = sdf.parse("31/12/" + y1);
+        Date start = sdf.parse("01/01/" + now);
+        Date end = sdf.parse("31/12/" + y2);
 
-        Date start1y = sdf.parse("01/01/" + y2);
-        Date end1y = sdf.parse("31/12/" + y2);
+        Logger.info("Getting citations...");
+        List<Citation> citations = Citation.find("created between ? and ?", end, start).fetch();
 
-        for (Journal journal : journals) {
-            Logger.info("****************");
+        int counter = 0;
+        int total = citations.size();
+
+        //Map holding the journal's journalAbbreviation and their citation counts.
+        HashMap<String, List<Integer>> citationMap = new HashMap<String, List<Integer>>();
+
+        for (Citation citation : citations) {
             counter++;
-            Logger.info(journal.title + ": " + counter + "/" + total);
-            //Get the citations for the preceeding year
-            List<Citation> citations1y = Citation.find("journalAbbreviation = ? and created between ? and ?", journal.issn, start1y, end1y).fetch();
-            //Logger.info("- Article " + y1 + ": " + citations1y.size());
-
-            //Get the citations for the second preceeding year
-            List<Citation> citations2y = Citation.find("journalAbbreviation = ? and created between ? and ?", journal.issn, start2y, end2y).fetch();
-            //Logger.info("- Article " + y2 + ": " + citations2y.size());
-
-            //check if the journal qualifies to receive an OpenIF value.
-            if (citations1y.isEmpty() || citations2y.isEmpty()) {
-                journal.isOldEnough = false;
-                Logger.info("- Not old enough");
-            } else {
-                journal.isOldEnough = true;
-            }
-
-            //Aggregate of the citations
-            List<Citation> citations = new ArrayList<Citation>();
-            citations.addAll(citations1y);
-            citations.addAll(citations2y);
-
-            //List holding the citation counts (= values)
-            List<Integer> citationCounts = new ArrayList<Integer>();
-            for (Citation citation : citations) {
+            Logger.info(counter + "/" + total);
+            if (citationMap.containsKey(citation.journalAbbreviation)) {
+                List<Integer> citationCounts = citationMap.get(citation.journalAbbreviation);
                 citationCounts.add(citation.citationCount);
+            } else {
+                List<Integer> citationCounts = new ArrayList<Integer>();
+                citationCounts.add(citation.citationCount);
+                citationMap.put(citation.journalAbbreviation, citationCounts);
             }
-
-            //Logger.info(citationCounts.toString());
-            double openIF = 0.0;
-            double deviationIF = 0.0;
-
-            //Compute the IF = mean citations per article published in the two previous years.
-            if (citations.size() > 0) {
-                openIF = (double) sum(citationCounts) / citations.size();
-            }
-            Logger.info("- IF: " + openIF);
-
-            //Compute the standard deviation of the sample (population)
-            double squaredDiff = 0.0;
-            for (Integer citationCount : citationCounts) {
-                double diff = Math.pow(citationCount - openIF, 2);
-                squaredDiff += diff;
-            }
-
-            if (citations.size() > 0) {
-                deviationIF = Math.sqrt(squaredDiff / citations.size());
-            }
-            Logger.info("- Deviation: " + deviationIF);
-
-            //Save the modifications
-            journal.openImpactFactor = openIF;
-            journal.deviationIF = deviationIF;
-            journal.save();
+            Logger.info(citation.journalAbbreviation + ": " + citationMap.get(citation.journalAbbreviation));
         }
         
+        //Save the jazz
+
+//            double openIF = 0.0;
+//            double deviationIF = 0.0;
+        //Compute the IF = mean citations per article published in the two previous years.
+//            if (citations.size() > 0) {
+//                openIF = (double) sum(citationCounts) / citations.size();
+//            }
+//            Logger.info("- IF: " + openIF);
+        //Compute the standard deviation of the sample (population)
+//            double squaredDiff = 0.0;
+//            for (Integer citationCount : citationCounts) {
+//                double diff = Math.pow(citationCount - openIF, 2);
+//                squaredDiff += diff;
+//            }
+//
+//            if (citations.size() > 0) {
+//                deviationIF = Math.sqrt(squaredDiff / citations.size());
+//            }
+//            Logger.info("- Deviation: " + deviationIF);
+        //Save the modifications
+//            journal.openImpactFactor = openIF;
+//            journal.deviationIF = deviationIF;
+//            journal.save();
         stopwatch.stop();
         Utils.emailAdmin("Stratified index built", "Job finished in " + stopwatch.elapsed(TimeUnit.MINUTES) + " minutes.");
     }
